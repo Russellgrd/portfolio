30feaa03b93273c428343017794898202acdab41
Russell Driver <driver.george85@gmail.com>
2021-08-08 09:58:13 +0100

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+
+    return (
+        <ul>
+            <li><Link to="">Home</Link></li>
+            <li><Link to="">About</Link></li>
+            <li><Link to="">Portfolio</Link></li>
+            <li><Link to="">Contact</Link></li>
+        </ul>
+    )
+}
+
+export default Nav;
