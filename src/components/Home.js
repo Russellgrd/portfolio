1b223bcff3de2322cc73bf6ad37adb01b6d01640
Russell Driver <driver.george85@gmail.com>
2021-08-12 08:04:22 +0100

    added styles to home page and added skills to textbox

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import code from '../images/code_img.png';
-import javascript from '../images/javascript_img.png';
-import css from '../images/css_img.png';
-import react from '../images/react_img.png';
+import code from '../images/code.png';
+import javascript from '../images/javascript.png';
+import css from '../images/css.png';
+import react from '../images/react.png';
 
 const Home = () => {
     return ( 
@@ -17,13 +17,20 @@ const Home = () => {
                     
                 <div className="Home-MainBox">
                     <div className="Home-MainBox-child Home-MainBox-textbox">
-                        <p>Lorem, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit eligendi vel omnis quasi possimus quos debitis, iure vero esse nobis beatae obcaecati ipsum maxime est minima dolore voluptatibus iusto culpa. Facilis nisi natus ab quis delectus ratione rerum aspernatur molestias voluptates minima, omnis et quae vel vitae at mollitia possimus, consectetur nobis odio rem libero illum explicabo consequuntur. Obcaecati laborum asperiores pariatur consectetur nihil. A ex repellendus mollitia ab iure quidem? Iusto delectus quos magni incidunt officiis, perspiciatis architecto eos cum blanditiis obcaecati aut necessitatibus voluptatum nemo vel, fugit et voluptates explicabo libero, ea consectetur. Impedit earum, quos ex incidunt ipsum ullam. Magni dicta cum debitis. Vel, exercitationem molestias ea fuga aperiam consequuntur quis sint culpa perferendis repellendus nam ex id impedit ratione delectus aspernatur, natus a architecto nisi. Expedita distinctio sed quas sint quam quasi corrupti reiciendis rem incidunt perspiciatis vero facilis quos quod aliquid aliquam, saepe nostrum necessitatibus accusantium odit eveniet magnam. Distinctio, nesciunt eos! Necessitatibus eaque reiciendis fugiat sit laborum recusandae distinctio laboriosam consectetur magni deserunt eligendi natus obcaecati atque quod non quis dolore id similique sequi, inventore cumque earum. Nesciunt iste excepturi quia nostrum accusantium qui, ducimus consequatur aliquid, necessitatibus explicabo eaque! Fugiat similique saepe soluta. ipsum dolor sit amet consectetur adipisicing elit. Sit id ipsa, nam eum ipsum sed iste dolorum autem consequatur cupiditate libero debitis possimus magnam culpa cumque enim sapiente inventore vitae!</p>
+                        <h2 className="Home-MainBox-textbox-p">Confident in:</h2>
+                        <p className="Home-MainBox-textbox-p">Javascript</p>
+                        <p className="Home-MainBox-textbox-p">html</p>
+                        <p className="Home-MainBox-textbox-p">css</p>  
+                        <p className="Home-MainBox-textbox-p">React</p>
+                        <p className="Home-MainBox-textbox-p">front end</p>
+                        <p className="Home-MainBox-textbox-p">backend</p>
+                        <p className="Home-MainBox-textbox-p">node, npm</p> 
+                        <p className="Home-MainBox-textbox-p">git</p>
+                        <p className="Home-MainBox-textbox-p">Visual Studio Code</p>
+                        <p className="Home-MainBox-textbox-p">expressjs</p>
+                        <p className="Home-MainBox-textbox-p">mongoose</p>
+                        <p className="Home-MainBox-textbox-p">mongodb</p>
                     </div>
-
-                    <div className="Home-MainBox-child Home-MainBox-textbox">
-                        <p>Lorem, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit eligendi vel omnis quasi possimus quos debitis, iure vero esse nobis beatae obcaecati ipsum maxime est minima dolore voluptatibus iusto culpa. Facilis nisi natus ab quis delectus ratione rerum aspernatur molestias voluptates minima, omnis et quae vel vitae at mollitia possimus, consectetur nobis odio rem libero illum explicabo consequuntur. Obcaecati laborum asperiores pariatur consectetur nihil. A ex repellendus mollitia ab iure quidem? Iusto delectus quos magni incidunt officiis, perspiciatis architecto eos cum blanditiis obcaecati aut necessitatibus voluptatum nemo vel, fugit et voluptates explicabo libero, ea consectetur. Impedit earum, quos ex incidunt ipsum ullam. Magni dicta cum debitis. Vel, exercitationem molestias ea fuga aperiam consequuntur quis sint culpa perferendis repellendus nam ex id impedit ratione delectus aspernatur, natus a architecto nisi. Expedita distinctio sed quas sint quam quasi corrupti reiciendis rem incidunt perspiciatis vero facilis quos quod aliquid aliquam, saepe nostrum necessitatibus accusantium odit eveniet magnam. Distinctio, nesciunt eos! Necessitatibus eaque reiciendis fugiat sit laborum recusandae distinctio laboriosam consectetur magni deserunt eligendi natus obcaecati atque quod non quis dolore id similique sequi, inventore cumque earum. Nesciunt iste excepturi quia nostrum accusantium qui, ducimus consequatur aliquid, necessitatibus explicabo eaque! Fugiat similique saepe soluta. ipsum dolor sit amet consectetur adipisicing elit. Sit id ipsa, nam eum ipsum sed iste dolorum autem consequatur cupiditate libero debitis possimus magnam culpa cumque enim sapiente inventore vitae!</p>
-                    </div>
-
                 </div>
         </div>
      );
