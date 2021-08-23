76aa26b20e4b7ac48b4c4c34b7f44e705e2fef8b
Russell Driver <driver.george85@gmail.com>
2021-08-23 08:12:54 +0100

    added png image to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import facepic from '../images/facepic.png';
-import desk from '../images/desk.jpg';
+import information from '../images/information.png';
+
 
 const About = () => {
 
@@ -15,8 +16,9 @@ const About = () => {
             <div className="AboutTextBox">
                 <p className="AboutTextBox-p">
                     I am a self taught web developer who is confident in Javascript, front-end and back-end server side programming looking for a complete career change into programming as I currently work full time in payroll HR for a London based pathology company, and am looking for an opportunity to work full time in web development.  I have been programming for 3.5 years, both on personal projects as well as private clients on evenings and over weekends.  I have learnt all the fundamentals of Javascript such as API fetch requests, handling response data structures and rendering to the browser as necessary, creating and dealing with objects either by function constructors or the newer class based syntax.  I have programmed in vanilla Javascript, as well as frameworks such as React, as well as using NPM packages along with React, such as express, ejs, mongoose, react-router-dom, and others.  
-                    <br /> <br />
+                </p>
 
+                <p className="AboutTextBox-p">
                     I am happy to join the company on a junior to intermediate level as this would be my first full time permanent developer role.
                     I am very familiar with GIT version control, push and pull requests, and programming in a methodical precise clean way, with regular commits in order for the code to be reviewed before for any main branch merges.
                     I am familiar with all ES6 syntax, as well as older syntax.  I understand closure, scope, the call stack as well as synchronous vs asynchronous code.
@@ -24,8 +26,10 @@ const About = () => {
                 </p>
 
                 <div className="AboutTextBox-imgDiv">
-                    <img className="AboutBox-imgDiv-img" src={desk} alt="desk with laptop" />
+                            <img className="AboutBox-imgDiv-img" src={information} alt="man on a laptop" />
+                            
                 </div>
+
             </div>
 
 
